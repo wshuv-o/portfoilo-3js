8079780e64d5cd1eb30905b31dec5ca644ec69f6
Md Wahiduzzaman Suva <100506316+wshuv-o@users.noreply.github.com>
2025-06-10 22:03:44 +0600

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Tilt} from "react-tilt";
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
-import { services } from '../constants';
+import { services } from '../constants'; 
 import{fadeIn, textVariant} from '../utils/motion';
 import { SectionWrapper } from '../hoc';
 
