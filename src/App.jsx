354d88f7a3661f43a850b1cfb81e62c725e61d49
Md Wahiduzzaman Suva <100506316+wshuv-o@users.noreply.github.com>
2025-06-30 23:16:59 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 const Home = () => (
-  <div className="relative z-0 bg-primary">
+  <div className="relative z-0 bg-primary"> 
     <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
       <Navbar />
       <Hero />
